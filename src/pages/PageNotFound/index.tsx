2d3f289283e787ed1cf4557e755b8fbf2e3fb640
laura-2 <laurahelenaverbinen@gmail.com>
2023-10-11 16:23:31 -0300

    change size text

--- src/pages/PageNotFound/index.tsx
+++ src/pages/PageNotFound/index.tsx
@@ -4,7 +4,7 @@ export default function PageNotFound(){
     return(
         <section className="flex justify-center items-center mt-24">
             <img alt="Error 404" src={page} className="w-6/12 lg:w-4/12"/>
-            <p className="text-5xl text-center mt-44 absolute z-10 uppercase textError">Error 404</p>
+            <p className="text-xl text-center mt-20 absolute z-10 uppercase">Error 404</p>
         </section>
     )
 }

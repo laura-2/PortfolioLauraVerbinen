1f760821a5a22423d4ec8890c3f4a009ca6532c5
laura-2 <laurahelenaverbinen@gmail.com>
2024-03-04 13:14:24 -0300

    change cv file

--- src/components/CV/index.tsx
+++ src/components/CV/index.tsx
@@ -6,7 +6,7 @@ interface CVProps {
 export default function CV({cvtitle}: CVProps) {
   return (
     <p className="questionDload">{cvtitle} 
-    <a download="LauraVerbinenEN.pdf" href="https://drive.google.com/file/d/1DEZ0rV4k59hOfTA2zpmRsFKu7v5tMPLP/view?usp=sharing"
+    <a download="LauraVerbinenEN.pdf" href="https://drive.google.com/file/d/18JVf5XlJtZ4mnfzThlv6-F28ex83gJQo/view?usp=sharing"
     className="dloadFile"> Click here</a></p>
   );
 }
